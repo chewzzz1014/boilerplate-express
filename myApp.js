7cefac3ea547c8863ca200ff6b6f2a92c8e47597
chewzzz <chewziqing@gmail.com>
2022-10-07 22:41:26 +0800

    Add GET

--- myApp.js
+++ myApp.js
@@ -1,6 +1,40 @@
 let express = require('express');
+let path = require("path");
+require("dotenv").config();
 let app = express();
 
+app.use(express.static(__dirname));
+// middleware
+app.use(function (req, res, next) {
+    // For every request,  log to the console a string taking the following format: method path - ip
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next();
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"));
+})
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase")
+        res.json({ "message": "HELLO JSON" });
+    else
+        res.json({ "message": "Hello json" });
+})
+app.get("/now", function (req, res, next) {  // middleware
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {    // handler
+    res.json({ "time": req.time });
+})
+app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({ "echo": word });
+})
+app.get("/name", (req, res) => {
+    const { first, last } = req.query;
+    res.json({ "name": `${first} ${last}` });
+})
+console.log("Hello World");
 
 
 
@@ -35,5 +69,4 @@ let app = express();
 
 
 
-
- module.exports = app;
+module.exports = app;
