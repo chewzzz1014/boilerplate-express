8b1320992d7212f252b9bcc5092caee0db7d9a43
chewzzz <chewziqing@gmail.com>
2022-10-11 21:15:19 +0800

    Add post request

--- myApp.js
+++ myApp.js
@@ -1,16 +1,18 @@
+const bodyParser = require('body-parser');
 let express = require('express');
 let path = require("path");
 require("dotenv").config();
 let app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // middleware
 app.use(function (req, res, next) {
     // For every request,  log to the console a string taking the following format: method path - ip
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next();
 })
-
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/index.html"));
 })
@@ -32,6 +34,10 @@ app.get("/:word/echo", (req, res) => {
 })
 app.get("/name", (req, res) => {
     const { first, last } = req.query;
+    res.json({ "name": `${first} ${last}` });
+})
+app.post("/name", (req, res) => {
+    const { first, last } = req.body;
     res.json({ "name": `${first} ${last}` });
 })
 console.log("Hello World");
